b15d53eb16d4c45bc67483450a49ce3ceb34bd8d
cristianoliveira <contato@cristianoliveira.com.br>
2025-04-17 12:37:12 +0200

    tests: split tests by checks

--- tests/test_bookmark_querier.py
+++ tests/test_bookmark_querier.py
@@ -85,6 +85,97 @@ class TestBookmarkQuerierFilterByFolders(unittest.TestCase):
                 }
             ]
         }
+
+        self.complex_tree_bookmark = {
+            "type": "folder",
+            "name": "root",
+            "children": [
+                {
+                    "type": "bookmark",
+                    "name": "git tutorial",
+                    "url": "https://git.org"
+                },
+                {
+                    "type": "folder",
+                    "name": "Python",
+                    "children": [
+                        {
+                            "type": "bookmark",
+                            "name": "Documentation",
+                            "url": "https://docs.python.org"
+                        },
+                        {
+                            "type": "bookmark",
+                            "name": "Python Tutorial",
+                            "url": "https://learn.python.org"
+                        },
+                        # Test subfolder
+                        {
+                            "type": "folder",
+                            "name": "Advanced",
+                            "children": [
+                                {
+                                    "type": "bookmark",
+                                    "name": "Documentation and Tutorial",
+                                    "url": "https://documentation.typescript.org"
+                                },
+                                {
+                                    "type": "bookmark",
+                                    "name": "Python Tricks Tutorial",
+                                    "url": "https://realpython.com"
+                                },
+                                {
+                                    "type": "bookmark",
+                                    "name": "Zen of Python",
+                                    "url": "https://peps.python.org/pep-0020/"
+                                }
+                            ]
+                        }
+
+                    ]
+                },
+                {
+                    "type": "folder",
+                    "name": "Typescript",
+                    "children": [
+                        {
+                            "type": "bookmark",
+                            "name": "Documentation",
+                            "url": "https://docs.typescript.org"
+                        },
+                        {
+                            "type": "bookmark",
+                            "name": "Typescript Tutorial",
+                            "url": "https://learn.typescript.org"
+                        },
+                        # Test subfolder
+                        {
+                            "type": "folder",
+                            "name": "Advanced",
+                            "children": [
+                                {
+                                    "type": "bookmark",
+                                    "name": "Documentation and Tutorial",
+                                    "url": "https://documentation.typescript.org"
+                                },
+                                {
+                                    "type": "bookmark",
+                                    "name": "Typescript Tricks Tutorial",
+                                    "url": "https://typescript.org"
+                                },
+                                {
+                                    "type": "bookmark",
+                                    "name": "Zen of Typescript",
+                                    "url": "https://notfound.com"
+                                }
+                            ]
+                        }
+
+                    ]
+                }
+            ]
+        }
+        
         self.querier = BookmarkQuerier(
             filter_by_folders=True
         )
@@ -132,104 +223,24 @@ class TestBookmarkQuerierFilterByFolders(unittest.TestCase):
         self.assertEqual(len(matches), 10)  # Should stop at max_matches_len 
 
     def test_search_matches_parent_folder(self):
-        bookmarks = {
-            "type": "folder",
-            "name": "root",
-            "children": [
-                {
-                    "type": "bookmark",
-                    "name": "git tutorial",
-                    "url": "https://git.org"
-                },
-                {
-                    "type": "folder",
-                    "name": "Python",
-                    "children": [
-                        {
-                            "type": "bookmark",
-                            "name": "Documentation",
-                            "url": "https://docs.python.org"
-                        },
-                        {
-                            "type": "bookmark",
-                            "name": "Python Tutorial",
-                            "url": "https://learn.python.org"
-                        },
-                        # Test subfolder
-                        {
-                            "type": "folder",
-                            "name": "Advanced",
-                            "children": [
-                                {
-                                    "type": "bookmark",
-                                    "name": "Python Tricks Tutorial",
-                                    "url": "https://realpython.com"
-                                },
-                                {
-                                    "type": "bookmark",
-                                    "name": "Zen of Python",
-                                    "url": "https://peps.python.org/pep-0020/"
-                                }
-                            ]
-                        }
-
-                    ]
-                },
-                {
-                    "type": "folder",
-                    "name": "Typescript",
-                    "children": [
-                        {
-                            "type": "bookmark",
-                            "name": "Documentation",
-                            "url": "https://docs.typescript.org"
-                        },
-                        {
-                            "type": "bookmark",
-                            "name": "Typescript Tutorial",
-                            "url": "https://learn.typescript.org"
-                        },
-                        # Test subfolder
-                        {
-                            "type": "folder",
-                            "name": "Advanced",
-                            "children": [
-                                {
-                                    "type": "bookmark",
-                                    "name": "Typescript Tricks Tutorial",
-                                    "url": "https://typescript.org"
-                                },
-                                {
-                                    "type": "bookmark",
-                                    "name": "Zen of Typescript",
-                                    "url": "https://notfound.com"
-                                }
-                            ]
-                        }
-
-                    ]
-                }
-            ]
-        }
-        
         matches = []
-        self.querier.search(bookmarks, "python documentation", matches)
+        self.querier.search(self.complex_tree_bookmark, "python documentation", matches)
         self.assertEqual(len(matches), 1)
         self.assertEqual(matches[0]["name"], "Documentation")
         self.assertEqual(matches[0]["url"], "https://docs.python.org")
 
         # Make sure to consider URL matches as well
         matches = []
-        self.querier.search(bookmarks, "python learn", matches)
-        
+        self.querier.search(self.complex_tree_bookmark, "python learn", matches)
         self.assertEqual(len(matches), 1)
         self.assertEqual(matches[0]["name"], "Python Tutorial")
         self.assertEqual(matches[0]["url"], "https://learn.python.org")
 
+    def test_filter_by_bookmark_name_independent_of_parent(self):
         # Test that it still works with just the bookmark name
         matches = []
-        self.querier.search(bookmarks, "documentation", matches)
-        self.assertEqual(len(matches), 2)
+        self.querier.search(self.complex_tree_bookmark, "documentation", matches)
+        self.assertEqual(len(matches), 4)
         # Matches python documentation
         self.assertEqual(matches[0]["name"], "Documentation")
         self.assertEqual(matches[0]["url"], "https://docs.python.org")
@@ -238,31 +249,42 @@ class TestBookmarkQuerierFilterByFolders(unittest.TestCase):
         self.assertEqual(matches[1]["url"], "https://docs.typescript.org")
         
 
+    def test_filter_multiple_folders_name(self):
+        # Test that it still works with just the bookmark name
         # Test that it matches folder and subfolder
         matches = []
-        self.querier.search(bookmarks, "python advanced", matches)
+        self.querier.search(self.complex_tree_bookmark, "python advanced", matches)
+        self.assertEqual(len(matches), 2)
+        matches = []
+        self.querier.search(self.complex_tree_bookmark, "typescript advanced", matches)
         self.assertEqual(len(matches), 2)
 
+    def test_filter_by_bookmark_name_only(self):
         matches = []
-        self.querier.search(bookmarks, "tutorial", matches)
-        self.assertEqual(len(matches), 5)
+        self.querier.search(self.complex_tree_bookmark, "tutorial", matches)
+        self.assertEqual(len(matches), 7)
         self.assertEqual(matches[0]["name"], "git tutorial")
+        self.assertEqual(matches[1]["name"], "Python Tutorial")
+        self.assertEqual(matches[2]["name"], "Python Tricks Tutorial")
+        self.assertEqual(matches[3]["name"], "Typescript Tutorial")
 
         matches = []
-        self.querier.search(bookmarks, "advanced tutorial", matches)
+        self.querier.search(self.complex_tree_bookmark, "advanced tutorial", matches)
         self.assertEqual(len(matches), 2)
         self.assertEqual(matches[0]["name"], "Python Tricks Tutorial")
 
+    def test_matches_children_and_grand_children(self):
         # Test that it matches when both parent and bookmark match
         # Should match: python/* && python/Advanced/*
         matches = []
-        self.querier.search(bookmarks, "python tutorial", matches)
+        self.querier.search(self.complex_tree_bookmark, "python tutorial", matches)
         self.assertEqual(len(matches), 2)
         self.assertEqual(matches[0]["name"], "Python Tutorial")
         self.assertEqual(matches[1]["name"], "Python Tricks Tutorial")
         
+    def test_filter_by_bookmark_name_and_parent_no_match(self):
         # Test that it doesn't match when neither parent nor bookmark match
         matches = []
-        self.querier.search(bookmarks, "java docs", matches)
+        self.querier.search(self.complex_tree_bookmark, "java docs", matches)
         self.assertEqual(len(matches), 0) 
 
